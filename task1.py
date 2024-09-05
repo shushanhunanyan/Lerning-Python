06f8f0ae206f9f3aa769ac0d1c8ab82316fc9198
Shushan Hunanyan <141552314+shushanhunanyan@users.noreply.github.com>
2024-09-05 16:48:35 +0400

    task1
    1. Write a function to perform basic arithmetic operations.
    2. Implement a Fibonacci Sequence Generator.
    3. Create a function that checks whether a given string is a palindrome(reads the same forward and backward).

---
+++ task1.py
@@ -0,0 +1,33 @@
+# 1. Basic arithmetic operations func
+def operations(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    else:
+        print("Invalid operation")
+
+
+
+# 2. Implement a Fibonacci Sequence Generator.
+def fibonacci_generator():
+    a = 0
+    b = 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+fib_gen = fibonacci_generator()
+
+for _ in range(10):
+    print(next(fib_gen))
+
+
+
+# 3. Checks whether a given string is a palindrome
+def is_palindrome(str):
+    str = str.replace(" ", "").lower()
+
+    return str == str[::-1]
